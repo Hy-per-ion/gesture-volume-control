57153c5fc19305f21d3e11582528c5f806a252aa
Kaushik <122469210+Hy-per-ion@users.noreply.github.com>
2023-08-22 19:04:26 +0530

    gvc v1.0

---
+++ volume_hand_control.py
@@ -0,0 +1,62 @@
+import cv2
+import time
+import numpy as np
+import HT_module as htm
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 1280, 640
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+volRange = volume.GetVolumeRange()
+# (-63.5, 0.0, 0.5)
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+detector = htm.handDetector(detectionCon=0.7)
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # 4 - thumb_tip & 8 - index_finger_tip
+    if len(lmList) != 0:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (0, 255, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        print(int(length), vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (75, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (75, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS:{int(fps)}', (40, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit the program
+        break
+cap.release()
+cv2.destroyAllWindows()
